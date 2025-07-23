ea4f5522a73f93be5c16cf1026a25ca32f614673
thesilph <the_silph@hotmail.com>
2025-07-23 18:20:27 +0100

    can call wallet, added piggy

---
+++ massa-piggy-dapp/assets/piggy.tsx
@@ -0,0 +1,26 @@
+export default function PiggyBank () {
+    return (
+    <svg
+    width="200"
+    height="200"
+    viewBox="0 0 200 200"
+    xmlns="http://www.w3.org/2000/svg"
+>
+
+    <rect x="50" y="80" width="100" height="80" fill="#ffcccb" />
+    <rect x="70" y="50" width="60" height="40" fill="#ffcccb" />
+
+    <rect x="80" y="90" width="10" height="10" fill="#000" />
+    <rect x="110" y="90" width="10" height="10" fill="#000" />
+
+    <rect x="95" y="110" width="10" height="5" fill="#000" />
+
+    <polygon points="70,50 60,40 70,40" fill="#ffcccb" />
+    <polygon points="130,50 140,40 130,40" fill="#ffcccb" />
+ 
+    <rect x="90" y="60" width="20" height="5" fill="#999" />
+
+    <rect x="60" y="160" width="10" height="10" fill="#ff9999" />
+    <rect x="130" y="160" width="10" height="10" fill="#ff9999" />
+</svg>
+    )}
